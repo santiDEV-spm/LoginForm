6d4455bdc5c523e282318cea86edd1312f542a06
santiDev <androidsan94@gmail.com>
2022-02-20 13:20:30 -0600

    2st

--- app/src/main/java/com/santidev/loginform/MainActivity.java
+++ app/src/main/java/com/santidev/loginform/MainActivity.java
@@ -3,6 +3,10 @@ package com.santidev.loginform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.form_layout);
-        
+        View view = (LinearLayout) findViewById(R.id.linear);
+        Snackbar.make(view, "Hola", Snackbar.LENGTH_LONG).show();
     }
 }
